ec5c637b7488df96d54c08b6707f8a2d2e7b020d
programmer0152 <abou.w@hotmail.com>
2020-03-09 19:35:28 +0100

    First full table.

--- Formatieve opdracht 2b.py
+++ Formatieve opdracht 2b.py
@@ -5,24 +5,32 @@ from pymongo import MongoClient
 limit = 100
 client = MongoClient('localhost', 27017)
 db = client["huwebshop"]
-collection = db["profiles"]
+collection = db["products"]
 
 cnx = mysql.connector.connect(user='root', password='root', host='127.0.0.1', port='3307', database='huwebshop')
 cursor = cnx.cursor()
 
-cursor.execute("DROP TABLE IF EXISTS profiles;")
+cursor.execute("DROP TABLE IF EXISTS products;")
 
-cursor.execute("CREATE TABLE profiles"
-                "(int_id int(11) PRIMARY KEY,"
-                "id char(24))"
+cursor.execute("CREATE TABLE products"
+                "(id char(24) PRIMARY KEY,"
+                "category char(24),"
+                "sub_category char(24),"
+                "sub_sub_category char(24),"
+                "gender char(8),"
+                "brand char(24),"
+                "mrsp int(10))"
                 "ENGINE=InnoDB")
 
-command = ("INSERT INTO profiles (int_id, id) VALUES (%s, %s)")
+command = ("INSERT INTO products (id, category, sub_category, sub_sub_category, gender, brand, mrsp) "
+           "VALUES (%s, %s, %s, %s, %s, %s, %s)")
 values = []
 
 counter = 0
-for profiles in collection.find():
-    values.append((counter, str(profiles["_id"])))
+for products in collection.find():
+    values.append((str(products["_id"]), str(products["category"]), str(products["sub_category"]),
+                   str(products["sub_sub_category"]), str(products["gender"]), str(products["brand"]),
+                   str(products["price"]["mrsp"])))
     counter += 1
     if counter == limit:
         print(f"{counter} entries uploaded.")

afd8902e98746bd20aaf6ec5b2309f373f060524
programmer0152 <abou.w@hotmail.com>
2020-03-10 10:46:43 +0100

    Prototype create table function.

--- Formatieve opdracht 2b.py
+++ Formatieve opdracht 2b.py
@@ -1,58 +1,52 @@
 import mysql.connector
 from pymongo import MongoClient
 
+cnx = mysql.connector.connect(user='root', password='root', host='127.0.0.1', port='3307', database='huwebshop')
+cursor = cnx.cursor()
 
 limit = 100
 client = MongoClient('localhost', 27017)
 db = client["huwebshop"]
-collection = db["products"]
-collection2 = db["profiles"]
-collection3 = db["sessions"]
 
-temp = 0
-for session in collection3.find():
-    temp = str(session["buid"][0])
-    break
 
-for profile in collection2.find():
-    try:
-        if temp == str(profile["buids"][0]):
-            print("FOUND!")
+def create_table(table_name, *args):
+    collection = db[table_name]
+    cursor.execute(f"DROP TABLE IF EXISTS {table_name};")
+
+    # Making the create table part dynamically with the arguments that are provided.
+    cursor_arg = f"CREATE TABLE {table_name}("
+    for arg in args:
+        cursor_arg += arg
+    cursor_arg += "ENGINE=InnoDB"
+    cursor.execute(cursor_arg)
+
+    # Making the insert table part dynamically with the arguments that are provided.
+    command_arg = f"INSERT INTO {table_name}("
+    for arg in range(len(args)):
+        sep = "," if arg < len(args) - 1 else ")"
+        command_arg += args[arg].split()[0] + sep
+    val_s = "VALUES (" + "%s," * len(args) + ")"
+    val_s = val_s[:-2] + val_s[-1]  # Removing the last comma to make the statement working.
+    command = (command_arg + val_s)
+
+    values = []
+    counter = 0
+    for entry in collection.find():
+        value_list = []
+        for arg in args:
+            value_list.append(entry[arg.split()[0]])
+        values.append(tuple(value_list))
+
+        counter += 1
+        if counter == limit:
+            print(f"{counter} entries of {table_name} uploaded.")
             break
-    except:
-        continue
+
+    cursor.executemany(command, values)
 
 
-cnx = mysql.connector.connect(user='root', password='root', host='127.0.0.1', port='3307', database='huwebshop')
-cursor = cnx.cursor()
-
-cursor.execute("DROP TABLE IF EXISTS products;")
-
-cursor.execute("CREATE TABLE products"
-                "(id char(28) PRIMARY KEY,"
-                "category char(43),"
-                "sub_category char(25),"
-                "sub_sub_category char(33),"
-                "gender char(15),"
-                "brand char(26),"
-                "mrsp int)"
-                "ENGINE=InnoDB")
-
-command = ("INSERT INTO products (id, category, sub_category, sub_sub_category, gender, brand, mrsp) "
-           "VALUES (%s, %s, %s, %s, %s, %s, %s)")
-values = []
-
-counter = 0
-for products in collection.find():
-    values.append((str(products["_id"]), str(products["category"]), str(products["sub_category"]),
-                   str(products["sub_sub_category"]), str(products["gender"]), str(products["brand"]),
-                   str(products["price"]["mrsp"])))
-    counter += 1
-    if counter == limit:
-        print(f"{counter} entries uploaded.")
-        break
-
-cursor.executemany(command, values)
+create_table("products", "_id char(28) PRIMARY KEY,", "category char(43),", "sub_category char(25),", "sub_sub_category char(33),",
+             "gender char(15),", "brand char(26))")
 
 cnx.commit()
 cnx.close()

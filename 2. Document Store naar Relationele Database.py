3b3cfa46c974ab85b7f9568a92f122ea9fa57dac
programmer0152 <abou.w@hotmail.com>
2020-03-11 16:29:09 +0100

    Normal table upload working.

--- 2. Document Store naar Relationele Database.py
+++ 2. Document Store naar Relationele Database.py
@@ -30,7 +30,44 @@ def make_normalized_table(table_name, db_name, search_value, id, collumn1):
     print(f"{table_name} table uploaded using {db_name} database.")
 
 
+def create_table(table_name, db_name, values, headers):
+    collection = db[db_name]
+    cursor.execute(f"DELETE FROM {table_name};")
+
+    counter = 0
+    upload_values = []
+    for entry in collection.find():
+        try:
+            upload = [counter]
+            for value in values:
+                if "-" in value:
+                    vl = value.split("-")
+                    upload.append(entry[vl[0]][vl[1]])
+                else:
+                    upload.append(entry[value])
+            upload_values.append(tuple(upload))
+            counter += 1
+        except KeyError:
+            continue
+
+        if counter == limit:
+            break
+
+    parameters = ""
+    value_string = ""
+    for header in headers:
+        parameters += header + ","
+        value_string += "%s,"
+    parameters = parameters[:-1]
+    value_string = value_string[:-1]
+
+    cursor.executemany(f"INSERT INTO {table_name}({parameters}) VALUES ({value_string})", upload_values)
+    cnx.commit()
+    print(f"{table_name} table uploaded using {db_name} database.")
+
+
 make_normalized_table("gender", "products", "gender", "idgender", "gendernaam")
 make_normalized_table("brand", "products", "brand", "idbrand", "brandnaam")
+create_table("profile", "profiles", ["order-count", "recommendations-segment"], ["id", "`order.count`", "recmonendation_segment"])
 
 cnx.close()

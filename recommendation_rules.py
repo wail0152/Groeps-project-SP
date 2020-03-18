2d3d64bcd7e0649a67a433a9dc4e79742664300f
programmer0152 <abou.w@hotmail.com>
2020-03-18 16:22:34 +0100

    Working prototype version. (#TODO: clean up the code).

--- recommendation_rules.py
+++ recommendation_rules.py
@@ -14,7 +14,7 @@ cur.execute("""CREATE TABLE recommendations
                  product4 VARCHAR);""")
 
 
-def similar(product_id, attributes):
+def similar_products(product_id, attributes):
     # Making the attribute in a format for the select statements.
     attribute_string = ""
     for attribute in attributes:
@@ -58,15 +58,63 @@ def insert_recommendations():
 
     # Getting all the ids of the similar products
     upload_values = []
-    for id in product_ids:
-        upload_values.append(list(id) + similar(id[0], ["subsubcategory", "targetaudience"]))
+    for product_id in product_ids:
+        upload_values.append(list(product_id) + similar_products(product_id[0], ["subsubcategory", "targetaudience"]))
 
     cur.executemany(f"""INSERT INTO recommendations (id, product1, product2, 
                         product3, product4)
                         VALUES (%s, %s, %s, %s, %s)""", upload_values)
 
 
-insert_recommendations()
+def similar_profile(profile_id, attributes):
+    # Making the attribute in a format for the select statements.
+    attribute_string = ""
+    for attribute in attributes:
+        attribute_string += attribute + ","
+    attribute_string = attribute_string[:-1]
+
+    # Check for similar profiles
+    cur.execute(f"""select {attribute_string} from sessions where profid = '{profile_id}';""")
+    info = list(cur.fetchall()[0])
+
+    # Pick random profile
+    conditions = ""
+    for index in range(len(info)):
+        conditions += attributes[index] + " = " + "'" + info[index] + "'"
+        if index < len(info) - 1:
+            conditions += " and "
+
+    cur.execute(f"""select profid from sessions where profid != '{profile_id}' and {conditions} and sale = true;""")
+    profiles = cur.fetchall()
+    rand_profile = [profile[0] for profile in random.sample(profiles, k=1)]
+
+    # Pick random product out of profile
+    cur.execute(f"""select prodid from profiles_previously_viewed where profid = '{rand_profile[0]}';""")
+    products_bought_ids = cur.fetchall()
+    rand_product_id = [product_id[0] for product_id in random.sample(products_bought_ids, k=1)]
+
+    # return db similar product id
+    return similar_products(rand_product_id[0], ["subsubcategory", "targetaudience"])
+
+
+def get_recommendation(profile_id):
+    cur.execute(f"""select sale from sessions where profid = '{profile_id}' and sale = true;""")    # Check if profile id has a sale in the sessions.
+    has_sale = cur.fetchall()
+    print("has sale:", has_sale != [])
+
+    if has_sale:    # if has_sale choose random product in the session and get a similar product via the database recommendation table.
+        cur.execute(f"""select prodid from profiles_previously_viewed where profid = '{profile_id}';""")
+        products_bought_ids = cur.fetchall()
+        rand_product_id = [product_id[0] for product_id in random.sample(products_bought_ids, k=1)]
+        return similar_products(rand_product_id[0], ["subsubcategory", "targetaudience"])
+    else:   # else check for a similar profile.
+        return similar_profile(profile_id, ["devicetype", "os"])
+
+
+# insert_recommendations()  #TODO: let this run once and then comment it out.
+print(get_recommendation("5a2df924a56ac6edb4feab38"))
+# (has sale) 5a2df924a56ac6edb4feab38
+# (doesn't have sale) 5ad7b06d89518600011831d0
 
 c.commit()
 cur.close()

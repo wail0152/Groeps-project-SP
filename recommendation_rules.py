4988e2b6d1ebe1b98e0b90518fa10105845d107c
programmer0152 <abou.w@hotmail.com>
2020-03-19 18:47:10 +0100

    Fully working version. (Next up: increase speed of uploading the profile_recommendations table).

--- recommendation_rules.py
+++ recommendation_rules.py
@@ -3,20 +3,32 @@ import random
 
 c = psycopg2.connect("dbname=postgres user=postgres password=postgres")
 cur = c.cursor()
-
-cur.execute("DROP TABLE IF EXISTS recommendations CASCADE")
-cur.execute("""CREATE TABLE recommendations (id VARCHAR PRIMARY KEY, product1 VARCHAR, 
-                product2 VARCHAR, product3 VARCHAR, product4 VARCHAR);""")
+counter = 0
+limit = 100
 
 
-def insert_recommendations():
-    cur.execute("""select id from products;""")
+def create_recommendation_table(table_name):
+    cur.execute(f"DROP TABLE IF EXISTS {table_name} CASCADE")
+    cur.execute(f"""CREATE TABLE {table_name} (id VARCHAR PRIMARY KEY, product1 VARCHAR,
+                    product2 VARCHAR, product3 VARCHAR, product4 VARCHAR);""")
+
+
+def insert_recommendations(select_table, insert_table, similar_def):
+    global counter
+    cur.execute(f"""select id from {select_table};""")
     pids = cur.fetchall()
 
-    # Getting all the ids of the similar products
-    upload_values = [list(pid) + similar_products(pid[0].replace("'", "''"), ["subsubcategory", "targetaudience"]) for pid in pids]
-    cur.executemany(f"""INSERT INTO recommendations (id, product1, product2, product3, product4)
-                        VALUES (%s, %s, %s, %s, %s)""", upload_values)
+    # Getting all the ids of the similar products and uploading them.
+    upload_values = []
+    for pid in pids:
+        upload_values.append(list(pid) + similar_def(pid[0].replace("'", "''"), ["subsubcategory", "targetaudience"]))
+        counter += 1
+        if counter >= limit:
+            break
+
+    cur.executemany(f"""INSERT INTO {insert_table} (id, product1, product2, product3, product4)
+                            VALUES (%s, %s, %s, %s, %s)""", upload_values)
+    print(f"Inserted the data of the {insert_table} table.")
 
 
 def similar_products(product_id, attributes):
@@ -40,7 +52,8 @@ def similar_products(product_id, attributes):
 def similar_profile(profile_id, attributes):
     # Check for similar profiles
     cur.execute(f"""select {get_attributes_query(attributes)} from sessions where profid = '{profile_id}';""")
-    properties_to_match = list(cur.fetchall()[0])
+    temp = cur.fetchall()
+    properties_to_match = list(temp[0])
 
     # Pick random profile
     conditions = get_conditions_query(attributes, properties_to_match)
@@ -76,24 +89,44 @@ def get_conditions_query(attributes, info):
 def get_recommendation_products(profile_id):
     cur.execute(f"""select prodid from profiles_previously_viewed where profid = '{profile_id}';""")
     bought_products_ids = cur.fetchall()
+
+    # This is to account for the imperfect data.
+    if not bought_products_ids:
+        return similar_profile(profile_id, ["devicetype", "os"])
+
     rand_product_id = [product_id[0] for product_id in random.sample(bought_products_ids, k=1)]
     return similar_products(rand_product_id[0], ["subsubcategory", "targetaudience"])
 
 
-def get_recommendation(profile_id):
+def get_recommendation(profile_id, placeholder):
+    # Check if profile is empty and then return None.
+    cur.execute(f"""select * from sessions where profid = '{profile_id}';""")
+    if not cur.fetchall():
+        return [profile_id, profile_id, profile_id, profile_id]
+
     # Check if profile id has a sale in the sessions.
     cur.execute(f"""select sale from sessions where profid = '{profile_id}' and sale = true;""")
     has_sale = cur.fetchall()
-    print("has sale:", has_sale != [])
 
     # if has_sale choose random product with profile id and get a similar product via the database recommendation table
     # else check for a similar profile.
     return get_recommendation_products(profile_id) if has_sale else similar_profile(profile_id, ["devicetype", "os"])
 
 
-# insert_recommendations()  #TODO: let this run once and then comment it out.
-# [debug purpose] (has sale: 5a2df924a56ac6edb4feab38) (doesn't have sale: 5ad7b06d89518600011831d0).
-print(get_recommendation("5ad7b06d89518600011831d0"))
+def get_recommendation_from_table(profile_id):
+    cur.execute(f"""select * from profile_recommendations where id = '{profile_id}';""")
+    recommendation = cur.fetchall()
+    return list(recommendation[0][1:])
+
+
+# TODO: let these four lines run once and then comment them out.
+# create_recommendation_table("product_recommendations")
+# create_recommendation_table("profile_recommendations")
+# insert_recommendations("products", "product_recommendations", similar_products)
+# insert_recommendations("profiles", "profile_recommendations", get_recommendation)
+# [debug purpose] (has sale: 5a393d68ed295900010384ca) (doesn't have sale: 5a394bebed2959000103972f).
+# print(get_recommendation_from_table("5a393d68ed295900010384ca"))
+
 
 c.commit()
 cur.close()
